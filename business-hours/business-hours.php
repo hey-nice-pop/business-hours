d66daef9f417f740fb0474b9e321647ec05ab0bb
hey nice pop <155707603+hey-nice-pop@users.noreply.github.com>
2024-03-29 10:37:35 +0900

    営業時間取得をget_posts使用に変更
    WP_Queryを使用して投稿を取得するとグローバルな$postを書き換えてしまうため、ページの他の投稿取得と競合が発生していた。

--- business-hours/business-hours.php
+++ business-hours/business-hours.php
@@ -10,43 +10,62 @@
 ?>
 <?php
 //メニューの追加
-add_action('admin_menu', function(){
-    add_menu_page(
+add_action('admin_menu', function () {
+	add_menu_page(
 		'営業時間追加' // ページのタイトルタグ<title>に表示されるテキスト
-		, '営業時間追加'   // 左メニューとして表示されるテキスト
-		, 'manage_options'       // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-		, 'business-hours'        // 左メニューのスラッグ名 →URLのパラメータに使われる /wp-admin/admin.php?page=toriaezu_menu
-		, '' // メニューページを表示する際に実行される関数(サブメニューがある時はこの値は空にする)
-		, 'dashicons-clock'       // メニューのアイコンを指定 https://developer.wordpress.org/resource/dashicons/#awards
-		, 0                             // メニューが表示される位置のインデックス(0が先頭) 5=投稿,10=メディア,20=固定ページ,25=コメント,60=テーマ,65=プラグイン,70=ユーザー,75=ツール,80=設定
+		,
+		'営業時間追加'   // 左メニューとして表示されるテキスト
+		,
+		'manage_options'       // 必要な権限 manage_options は通常 administrator のみに与えられた権限
+		,
+		'business-hours'        // 左メニューのスラッグ名 →URLのパラメータに使われる /wp-admin/admin.php?page=toriaezu_menu
+		,
+		'' // メニューページを表示する際に実行される関数(サブメニューがある時はこの値は空にする)
+		,
+		'dashicons-clock'       // メニューのアイコンを指定 https://developer.wordpress.org/resource/dashicons/#awards
+		,
+		0                             // メニューが表示される位置のインデックス(0が先頭) 5=投稿,10=メディア,20=固定ページ,25=コメント,60=テーマ,65=プラグイン,70=ユーザー,75=ツール,80=設定
 	);
 
-    add_submenu_page(
+	add_submenu_page(
 		'business-hours'    // 親メニューのスラッグ
-		, '使い方' // ページのタイトルタグ<title>に表示されるテキスト
-		, '使い方' // サブメニューとして表示されるテキスト
-		, 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-		, 'business-hours'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-		, 'howtobh' //（任意）このページのコンテンツを出力するために呼び出される関数
-		, 0
+		,
+		'使い方' // ページのタイトルタグ<title>に表示されるテキスト
+		,
+		'使い方' // サブメニューとして表示されるテキスト
+		,
+		'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
+		,
+		'business-hours'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
+		,
+		'howtobh' //（任意）このページのコンテンツを出力するために呼び出される関数
+		,
+		0
 	);
 
-    add_submenu_page(
+	add_submenu_page(
 		'business-hours'    // 親メニューのスラッグ
-		, '営業時間追加' // ページのタイトルタグ<title>に表示されるテキスト
-		, '営業時間追加' // サブメニューとして表示されるテキスト
-		, 'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
-		, 'add-business-hours'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
-		, 'hours_page_contents' //（任意）このページのコンテンツを出力するために呼び出される関数
-		, 1
+		,
+		'営業時間追加' // ページのタイトルタグ<title>に表示されるテキスト
+		,
+		'営業時間追加' // サブメニューとして表示されるテキスト
+		,
+		'manage_options' // 必要な権限 manage_options は通常 administrator のみに与えられた権限
+		,
+		'add-business-hours'  // サブメニューのスラッグ名。この名前を親メニューのスラッグと同じにすると親メニューを押したときにこのサブメニューを表示します。一般的にはこの形式を採用していることが多い。
+		,
+		'hours_page_contents' //（任意）このページのコンテンツを出力するために呼び出される関数
+		,
+		1
 	);
 });
 
 //=================================================
 // 表示・更新処理
 //=================================================
-function howtobh() {
-	$plugindir=plugin_dir_url( __FILE__ );
+function howtobh()
+{
+	$plugindir = plugin_dir_url(__FILE__);
 	//---------------------------------
 	// HTML表示
 	//---------------------------------
@@ -92,44 +111,44 @@ EOF;
 }
 
 //メニューを開いた時に実行
-function hours_page_contents() {
+function hours_page_contents()
+{
 	//---------------------------------
 	// ユーザーが必要な権限を持つか確認
 	//---------------------------------
-	if (!current_user_can('manage_options'))
-	{
-		wp_die( __('この設定ページのアクセス権限がありません') );
+	if (!current_user_can('manage_options')) {
+		wp_die(__('この設定ページのアクセス権限がありません'));
 	}
 
 	//---------------------------------
 	// 初期化
 	//---------------------------------
 
-	$sun = get_option('sun','');
-	$mon = get_option('mon','');
-	$tue = get_option('tue','');
-	$wed = get_option('wed','');
-	$thu = get_option('thu','');
-	$fri = get_option('fri','');
-	$sat = get_option('sat','');
+	$sun = get_option('sun', '');
+	$mon = get_option('mon', '');
+	$tue = get_option('tue', '');
+	$wed = get_option('wed', '');
+	$thu = get_option('thu', '');
+	$fri = get_option('fri', '');
+	$sat = get_option('sat', '');
 
-	$check_sun = get_option('check_sun','');
-	$check_mon = get_option('check_mon','');
-	$check_tue = get_option('check_tue','');
-	$check_wed = get_option('check_wed','');
-	$check_thu = get_option('check_thu','');
-	$check_fri = get_option('check_fri','');
-	$check_sat = get_option('check_sat','');
+	$check_sun = get_option('check_sun', '');
+	$check_mon = get_option('check_mon', '');
+	$check_tue = get_option('check_tue', '');
+	$check_wed = get_option('check_wed', '');
+	$check_thu = get_option('check_thu', '');
+	$check_fri = get_option('check_fri', '');
+	$check_sat = get_option('check_sat', '');
 
-	$bh_prefix = get_option('bh_prefix','');
-	$holiday = get_option('holiday','');
+	$bh_prefix = get_option('bh_prefix', '');
+	$holiday = get_option('holiday', '');
 
 	$message_html = "";
 
 	//---------------------------------
 	// 更新されたときの処理
 	//---------------------------------
-	if( isset($_POST[ 'submit' ])) {
+	if (isset($_POST['submit'])) {
 
 		//営業時間を更新
 		if (isset($_POST['sun'])) {
@@ -157,44 +176,37 @@ function hours_page_contents() {
 		//チェックボックスの値を更新
 		if (isset($_POST['check_sun'])) {
 			update_option('check_sun', $_POST['check_sun']);
-		}
-		else{
+		} else {
 			delete_option('check_sun');
 		}
 		if (isset($_POST['check_mon'])) {
 			update_option('check_mon', $_POST['check_mon']);
-		}
-		else{
+		} else {
 			delete_option('check_mon');
 		}
 		if (isset($_POST['check_tue'])) {
 			update_option('check_tue', $_POST['check_tue']);
-		}
-		else{
+		} else {
 			delete_option('check_tue');
 		}
 		if (isset($_POST['check_wed'])) {
 			update_option('check_wed', $_POST['check_wed']);
-		}
-		else{
+		} else {
 			delete_option('check_wed');
 		}
 		if (isset($_POST['check_thu'])) {
 			update_option('check_thu', $_POST['check_thu']);
-		}
-		else{
+		} else {
 			delete_option('check_thu');
 		}
 		if (isset($_POST['check_fri'])) {
 			update_option('check_fri', $_POST['check_fri']);
-		}
-		else{
+		} else {
 			delete_option('check_fri');
 		}
 		if (isset($_POST['check_sat'])) {
 			update_option('check_sat', $_POST['check_sat']);
-		}
-		else{
+		} else {
 			delete_option('check_sat');
 		}
 
@@ -208,27 +220,27 @@ function hours_page_contents() {
 		}
 
 		//更新後の再取得
-		$sun = get_option('sun','');
-		$mon = get_option('mon','');
-		$tue = get_option('tue','');
-		$wed = get_option('wed','');
-		$thu = get_option('thu','');
-		$fri = get_option('fri','');
-		$sat = get_option('sat','');
+		$sun = get_option('sun', '');
+		$mon = get_option('mon', '');
+		$tue = get_option('tue', '');
+		$wed = get_option('wed', '');
+		$thu = get_option('thu', '');
+		$fri = get_option('fri', '');
+		$sat = get_option('sat', '');
 
-		$check_sun = get_option('check_sun','');
-		$check_mon = get_option('check_mon','');
-		$check_tue = get_option('check_tue','');
-		$check_wed = get_option('check_wed','');
-		$check_thu = get_option('check_thu','');
-		$check_fri = get_option('check_fri','');
-		$check_sat = get_option('check_sat','');
+		$check_sun = get_option('check_sun', '');
+		$check_mon = get_option('check_mon', '');
+		$check_tue = get_option('check_tue', '');
+		$check_wed = get_option('check_wed', '');
+		$check_thu = get_option('check_thu', '');
+		$check_fri = get_option('check_fri', '');
+		$check_sat = get_option('check_sat', '');
 
-		$bh_prefix = get_option('bh_prefix','');
-		$holiday = get_option( 'holiday', '' );
+		$bh_prefix = get_option('bh_prefix', '');
+		$holiday = get_option('holiday', '');
 
 		// 画面にメッセージを表示
-		$message_html =<<<EOF
+		$message_html = <<<EOF
 			
 <div class="notice notice-success is-dismissible">
 	<p>
@@ -237,12 +249,12 @@ function hours_page_contents() {
 </div>
 			
 EOF;
-}
+	}
 
-//---------------------------------
-// HTML表示
-//---------------------------------
-echo $html =<<<EOF
+	//---------------------------------
+	// HTML表示
+	//---------------------------------
+	echo $html = <<<EOF
 
 {$message_html}
 
@@ -295,132 +307,135 @@ EOF;
 add_action('init', 'create_rinji_post_type');
 function create_rinji_post_type()
 {
-    //投稿時に使用できる投稿用のパーツを指定
-    $supports = array();
-    register_post_type(
-        'rinji', // 投稿タイプ名の定義
-        [
-        'labels' => [
-            'name' => '臨時営業時間', // 管理画面上で表示する投稿タイプ名
-        ],
-        'public'        => true,  // カスタム投稿タイプの表示(trueにする)
-        'exclude_from_search' => true,  //trueで検索結果から外す
-        'has_archive'   => false, // カスタム投稿一覧(true:表示/false:非表示)
-        'menu_position' => 1,     // 管理画面上での表示位置
-        'show_in_rest'  => false,  // true:「Gutenberg」/ false:「ClassicEditor」
-        'supports' => $supports
-        ]
-    );
+	//投稿時に使用できる投稿用のパーツを指定
+	$supports = array();
+	register_post_type(
+		'rinji', // 投稿タイプ名の定義
+		[
+			'labels' => [
+				'name' => '臨時営業時間', // 管理画面上で表示する投稿タイプ名
+			],
+			'public'        => true,  // カスタム投稿タイプの表示(trueにする)
+			'exclude_from_search' => true,  //trueで検索結果から外す
+			'has_archive'   => false, // カスタム投稿一覧(true:表示/false:非表示)
+			'menu_position' => 1,     // 管理画面上での表示位置
+			'show_in_rest'  => false,  // true:「Gutenberg」/ false:「ClassicEditor」
+			'supports' => $supports
+		]
+	);
 }
 
 //臨時営業時間カスタムフィールド追加
 add_action('admin_menu', 'create_rinji_custom_fields');
 function create_rinji_custom_fields()
 {
-    add_meta_box(
-        'rinji_setting', //編集画面セクションID
-        '臨時営業時間設定', //編集画面セクションのタイトル
-        'insert_rinji_custom_fields', //編集画面セクションにHTML出力する関数
-        'rinji', //投稿タイプ名
-        'normal' //編集画面セクションが表示される部分
-    );
+	add_meta_box(
+		'rinji_setting', //編集画面セクションID
+		'臨時営業時間設定', //編集画面セクションのタイトル
+		'insert_rinji_custom_fields', //編集画面セクションにHTML出力する関数
+		'rinji', //投稿タイプ名
+		'normal' //編集画面セクションが表示される部分
+	);
 }
 
 function insert_rinji_custom_fields()
 {
-    global $post;
-    $jikan = get_post_meta($post->ID, 'jikan', true);
-    if (get_post_meta($post->ID, 'yasumi', true) == "checked") {
-      $yasumi_check = "checked";
-    }
-    else{
-        $yasumi_check="";
-    }
-    ?>
-<form method="post" action="admin.php?page=site_settings">
-    <label for="jikan">時間：</label>
-    <input id="jikan" type="text" name="jikan" value="<?php echo $jikan ?>"><br><br>
-    <label for="yasumi">休みならここにチェック</label>
-    <input id="yasumi" type="checkbox" name="yasumi" value="checked" <?php echo $yasumi_check ?>><br><br>
-</form>
+	global $post;
+	$jikan = get_post_meta($post->ID, 'jikan', true);
+	if (get_post_meta($post->ID, 'yasumi', true) == "checked") {
+		$yasumi_check = "checked";
+	} else {
+		$yasumi_check = "";
+	}
+?>
+	<form method="post" action="admin.php?page=site_settings">
+		<label for="jikan">時間：</label>
+		<input id="jikan" type="text" name="jikan" value="<?php echo $jikan ?>"><br><br>
+		<label for="yasumi">休みならここにチェック</label>
+		<input id="yasumi" type="checkbox" name="yasumi" value="checked" <?php echo $yasumi_check ?>><br><br>
+	</form>
 <?php
 }
 add_action('save_post', 'save_rinji_custom_fields');
 //更新時の処理
 function save_rinji_custom_fields($post_id)
 {
-    if (isset($_POST['jikan'])) {
-        update_post_meta($post_id, 'jikan', $_POST['jikan']);
-    }
-    if (isset($_POST['yasumi'])) {
-        update_post_meta($post_id, 'yasumi', $_POST['yasumi']);
-    } else {
-        delete_post_meta($post_id, 'yasumi');
-    }
+	if (isset($_POST['jikan'])) {
+		update_post_meta($post_id, 'jikan', $_POST['jikan']);
+	}
+	if (isset($_POST['yasumi'])) {
+		update_post_meta($post_id, 'yasumi', $_POST['yasumi']);
+	} else {
+		delete_post_meta($post_id, 'yasumi');
+	}
 }
 
-add_action( 'init' , 'remove_rinji_editor_support' );
-function remove_rinji_editor_support() {
- remove_post_type_support( 'rinji', 'editor' );//本文機能を削除
+add_action('init', 'remove_rinji_editor_support');
+function remove_rinji_editor_support()
+{
+	remove_post_type_support('rinji', 'editor'); //本文機能を削除
 }
 
 //rinjiをnoindex
-function archive_noindex(){ 
- 
-    if ( is_post_type_archive( 'rinji' )||is_singular('movie')) {
-        echo '<meta name="robots" content="noindex , nofollow" />';
-    }
+function archive_noindex()
+{
+
+	if (is_post_type_archive('rinji') || is_singular('movie')) {
+		echo '<meta name="robots" content="noindex , nofollow" />';
+	}
 }
 add_action('wp_head', 'archive_noindex');
 
 //タイトル入力欄のプレースホルダー文言変更
-function change_default_title( $title ) {
-  $screen = get_current_screen();
-  if ( $screen->post_type == 'rinji' ) {
-        $title = 'ここに変更する日付を入力(店舗側確認用)';
-  }
-    return $title;
-  }
-  add_filter( 'enter_title_here', 'change_default_title' );
+function change_default_title($title)
+{
+	$screen = get_current_screen();
+	if ($screen->post_type == 'rinji') {
+		$title = 'ここに変更する日付を入力(店舗側確認用)';
+	}
+	return $title;
+}
+add_filter('enter_title_here', 'change_default_title');
 ?>
 <?php
 //ショートコード
 
-function business_hours(){
-    //今日の曜日を取得
+function business_hours()
+{
+	//今日の曜日を取得
 	$todayweek = date('w');
 
-    //出力用の時間や文言の取得
-    $message_holiday = get_option( 'holiday', '' );
-    $message_bh_prefix = get_option( 'bh_prefix', '' );
+	//出力用の時間や文言の取得
+	$message_holiday = get_option('holiday', '');
+	$message_bh_prefix = get_option('bh_prefix', '');
 
-	if($todayweek=="0"){
-		$youbi = get_option('sun','');
-        $check_holiday = get_option( 'check_sun', '' );
+	if ($todayweek == "0") {
+		$youbi = get_option('sun', '');
+		$check_holiday = get_option('check_sun', '');
 	}
-	if($todayweek=="1"){
-		$youbi = get_option('mon','');
-        $check_holiday = get_option( 'check_mon', '' );
+	if ($todayweek == "1") {
+		$youbi = get_option('mon', '');
+		$check_holiday = get_option('check_mon', '');
 	}
-	if($todayweek=="2"){
-		$youbi = get_option('tue','');
-        $check_holiday = get_option( 'check_tue', '' );
+	if ($todayweek == "2") {
+		$youbi = get_option('tue', '');
+		$check_holiday = get_option('check_tue', '');
 	}
-	if($todayweek=="3"){
-		$youbi = get_option('wed','');
-        $check_holiday = get_option( 'check_wed', '' );
+	if ($todayweek == "3") {
+		$youbi = get_option('wed', '');
+		$check_holiday = get_option('check_wed', '');
 	}
-	if($todayweek=="4"){
-		$youbi = get_option('thu','');
-        $check_holiday = get_option( 'check_thu', '' );
+	if ($todayweek == "4") {
+		$youbi = get_option('thu', '');
+		$check_holiday = get_option('check_thu', '');
 	}
-	if($todayweek=="5"){
-		$youbi = get_option('fri','');
-        $check_holiday = get_option( 'check_fri', '' );
+	if ($todayweek == "5") {
+		$youbi = get_option('fri', '');
+		$check_holiday = get_option('check_fri', '');
 	}
-	if($todayweek=="6"){
-		$youbi = get_option('sat','');
-        $check_holiday = get_option( 'check_sat', '' );
+	if ($todayweek == "6") {
+		$youbi = get_option('sat', '');
+		$check_holiday = get_option('check_sat', '');
 	}
 
 	//臨時営業時間用のクエリ作成
@@ -430,46 +445,41 @@ function business_hours(){
 		'posts_per_page' => 1,
 		'date_query' => array(
 			array(
-				'year'  => $today['year'],
+				'year' => $today['year'],
 				'month' => $today['mon'],
-				'day'   => $today['mday'],
-			)
-		)
+				'day' => $today['mday'],
+			),
+		),
 	);
-	$query = new WP_Query( $args );
 
-	// The Loop
-	if ( $query->have_posts() ) {
+	// get_postsでクエリ実行
+	$posts = get_posts($args);
 
-			$query->the_post();
-			
-			if (get_post_meta(get_the_ID(), 'yasumi', true) == "checked") {//休みにチェックが入っていたら
-			  return "<span id='message_holiday'>".$message_holiday."</span>";
-			}
-			else{//臨時時間に入力があった時(休みチェックなし)
-				if(get_post_meta(get_the_ID(), 'jikan', true)) {
-					$jikan = get_post_meta(get_the_ID(), 'jikan', true);
-
-					return "<span id='message_bh_prefix'>".$message_bh_prefix."</span><span id='bh_time'>".$jikan."</span>";
+	// 臨時営業時間の有無を確認
+	if (!empty($posts)) {
+		foreach ($posts as $post) {
+			setup_postdata($post);
+			// 臨時営業時間の情報取得と表示処理
+			if (get_post_meta($post->ID, 'yasumi', true) == "checked") {
+				wp_reset_postdata();
+				return "<span id='message_holiday'>" . $message_holiday . "</span>";
+			} else {
+				$jikan = get_post_meta($post->ID, 'jikan', true);
+				wp_reset_postdata();
+				if (!empty($jikan)) {
+					return "<span id='message_bh_prefix'>" . $message_bh_prefix . "</span><span id='bh_time'>" . $jikan . "</span>";
+				} else {
+					return "<span id='message_bh_prefix'>" . $message_bh_prefix . "</span><span id='bh_time'>" . $youbi . "</span>";
 				}
-				else{
-					return "<span id='message_bh_prefix'>".$message_bh_prefix."</span><span id='bh_time'>".$youbi."</span>";//基本診療時間の出力(臨時投稿は設定されているが休みにチェックなく、営業時間入ってないとき)
-				}
 			}
-
-		wp_reset_postdata();//臨時のpostdata消去
-	}//臨時営業時間の取得・判定ここまで
-	else{
-		wp_reset_postdata();//臨時のpostdata消去
-
-        if($check_holiday == "checked"){//曜日に休日チェックが入っていたら
-            return "<span id='message_holiday'>".$message_holiday."</span>";
-        }
-        else
-        {
-            return "<span id='message_bh_prefix'>".$message_bh_prefix."</span><span id='bh_time'>".$youbi."</span>";//基本営業時間の出力
-        }
-		
+		}
+	} else {
+		// 臨時営業時間が設定されていない場合、基本営業時間または休業情報の表示
+		if ($check_holiday == "checked") {
+			return "<span id='message_holiday'>" . $message_holiday . "</span>";
+		} else {
+			return "<span id='message_bh_prefix'>" . $message_bh_prefix . "</span><span id='bh_time'>" . $youbi . "</span>";
+		}
 	}
 }
 add_shortcode('business_hours', 'business_hours');
